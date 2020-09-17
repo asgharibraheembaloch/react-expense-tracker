eb1477127ce96bbadd3c7648efb1311807bec6f5
asgharibraheembaloch <asgharibraheembaloch@gmail.com>
2020-09-17 17:46:07 +0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Header} from './components/Header';
 /* import logo from './logo.svg';
 import './App.css'; */
 
 function App() {
   return (
     <div>
-      Hello from expense tracker app      
+      <Header />      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return(
+        <h1>
+            React Tracker App By Asghar Ibraheem Baloch
+        </h1>
+    )
+}
